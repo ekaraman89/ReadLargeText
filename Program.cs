3d6e787ce84d177d3c411984004af7e1f6e49da0
emrah karaman <ekaraman89@hotmail.com>
2021-02-01 16:54:11 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start...");
+
+            string path ="D:\\ArchiveValues.json";
+          
+
+            const Int32 BufferSize = 128;
+            using (var fileStream = File.OpenRead(path))
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize)) {
+                String line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    Console.Write(line);
+                    Console.ReadLine();
+                }
+            }
+
+        }
+    }
+}
